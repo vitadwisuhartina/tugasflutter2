1da3af2009b8cb5e71b063caaaadc6b9877383cb
vitadwisuhartina <137508454+vitadwisuhartina@users.noreply.github.com>
2023-10-09 09:53:39 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:form_produk/ui/produk_form.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Form Produk",
+      home: ProdukForm(),
+    );
+  }
+}
+
